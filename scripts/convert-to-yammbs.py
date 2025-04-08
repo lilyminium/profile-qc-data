08e42b05ced17c500555bbf0ca3b837fa08192ab
Lily Wang <lily@mdanalysis.org>
2025-04-09 06:16:11 +1000

    create own verbose function

--- scripts/convert-to-yammbs.py
+++ scripts/convert-to-yammbs.py
@@ -17,6 +17,40 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
+def create_moleculestore_verbose(dataset, dataset_name):
+    from yammbs.models import MoleculeRecord, QMConformerRecord
+    from yammbs._molecule import _smiles_to_inchi_key
+
+    store = MoleculeStore(dataset_name)
+
+    # only store unique molecule records
+    input_molecule_records = []
+    for qm_molecule in tqdm.tqdm(dataset.qm_molecules, desc="Creating MoleculeRecords"):
+        molecule_record = MoleculeRecord(
+            mapped_smiles=qm_molecule.mapped_smiles,
+            inchi_key=_smiles_to_inchi_key(qm_molecule.mapped_smiles),
+        )
+        input_molecule_records.append(molecule_record)
+    
+    store.store(input_molecule_records)
+
+    # now store QM records all at once
+    input_qm_records = []
+    for qm_molecule in tqdm.tqdm(dataset.qm_molecules, desc="Creating QMConformerRecords"):
+        qm_conformer_record = QMConformerRecord(
+            molecule_id=store.get_molecule_id_by_smiles(
+                qm_molecule.mapped_smiles,
+            ),
+            qcarchive_id=qm_molecule.qcarchive_id,
+            mapped_smiles=qm_molecule.mapped_smiles,
+            coordinates=qm_molecule.coordinates,
+            energy=qm_molecule.final_energy,
+        )
+        input_qm_records.append(qm_conformer_record)
+    
+    store.store_qcarchive(input_qm_records)
+
+
 def main(
     root_directory=".."
 ):
@@ -40,9 +74,13 @@ def main(
         overall_dataset.qm_molecules.extend(sub_qcarchive_dataset.qm_molecules)
     
     store_file = root_directory / "offqcdata/data/yammbs/optimizations.sqlite"
-    optimization_store = MoleculeStore.from_qm_dataset(
+    # optimization_store = MoleculeStore.from_qm_dataset(
+    #     dataset=overall_dataset,
+    #     database_name=store_file,
+    # )
+    create_moleculestore_verbose(
         dataset=overall_dataset,
-        database_name=store_file,
+        dataset_name=store_file,
     )
     logger.info(f"Wrote to {store_file}")
 
